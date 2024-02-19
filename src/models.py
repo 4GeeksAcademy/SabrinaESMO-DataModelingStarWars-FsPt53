f0b7bca1d40e7fa0920dca5b65930547ab72d422
Sabrina Estévez <sesmo.dev@gmail.com>
2024-02-19 20:10:00 +0000

    probando acabado

--- src/models.py
+++ src/models.py
@@ -7,26 +7,53 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User (Base):
+    __tablename__='User'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_name = Column(String(20), index=True)
+    first_name = Column(String(20))
+    last_name = Column(String(20))
+    email = Column(String(40), nullable=False)
+    fav_planet_id = Column(Integer)
+    fav_film_id = Column(Integer)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planets (Base):
+    __tablename__='Planets'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(50))
 
-    def to_dict(self):
-        return {}
+class Films (Base):
+    __tablename__='Films'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+
+class Vehicles (Base):
+    __tablename__='Vehicles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+
+class FavoritePlanets (Base):
+    __tablename__='FavoritePlanets'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('User.id'))
+    planets_id = Column(ForeignKey('Planets.id'))
+    planets = relationship(Planets)
+
+class FavoriteFilms (Base):
+    __tablename__='FavoriteFilms'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('User.id'))
+    films_id = Column(ForeignKey('Films.id'))
+    films = relationship(Films)
+
+class FavoriteVehicles (Base):
+    __tablename__='FavoriteVehicles'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('User.id'))
+    vehicles_id = Column(ForeignKey('vehicles.id'))
+    vehicles = relationship(Vehicles)
+
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
